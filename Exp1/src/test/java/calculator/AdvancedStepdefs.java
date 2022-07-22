c323dfd847e51c2e24b84a0452f8a31698d3874d
97102044 <ah.abbasi.ah@gmail.com>
2022-07-22 16:43:18 +0430

    complete adv calc and step d

--- Exp1/src/test/java/calculator/AdvancedStepdefs.java
+++ Exp1/src/test/java/calculator/AdvancedStepdefs.java
@@ -7,5 +7,31 @@ import cucumber.api.java.en.When;
 import org.junit.Assert;
 
 public class AdvancedStepdefs {
+    private AdvanceCalculator calculator;
+    private int value1;
+    private int value2;
+    private double result;
+
+    @Before
+    public void before() {
+        calculator = new AdvanceCalculator();
+    }
+
+    @Given("^Two input values, (-?\\d+) and (-?\\d+)$")
+    public void advTwoInputValuesAnd(int arg0, int arg1) {
+        value1 = arg0;
+        value2 = arg1;
+    }
+
+    @When("^I ([*/^]) the two values$")
+    public void iTheTwoValues(String operator) {
+        result = calculator.Calculate(value1, value2, operator);
+        System.out.println(result);
+    }
+
+    @Then("^I Expect the result (.+)$")
+    public void iExpectTheResult(String arg0) {
+        Assert.assertEquals(arg0, String.format("%,.1f", result));
+    }
 
 }
