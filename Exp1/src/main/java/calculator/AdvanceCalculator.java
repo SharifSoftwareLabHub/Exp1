040c45d1d7461e778aeb240a48bd1892d35c10de
97102044 <ah.abbasi.ah@gmail.com>
2022-07-22 15:23:26 +0430

    complete advance calc feature and java code

--- Exp1/src/main/java/calculator/AdvanceCalculator.java
+++ Exp1/src/main/java/calculator/AdvanceCalculator.java
@@ -1,20 +1,37 @@
 package calculator;
 
-public class AdvanceCalculator {
-    public static final String MulOp = "*";
-    public static final String PowOp = "^";
-    public static final String divOp = "/";
-    public double Calculate (int a, int b, String operator) {
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
 
-        switch (operator){
-            case MulOp:
-                return a*b;
-            case PowOp:
-                return Math.pow(a, b);
-            case divOp:
-                return (double)a / (double)b;
-            default:
-                throw new IllegalStateException(String.format("Unexpected value: %s", operator));
-        }
+public class AdvancedStepdefs {
+    private AdvanceCalculator calculator;
+    private int value1;
+    private int value2;
+    private double result;
+
+    @Before
+    public void before() {
+        calculator = new AdvanceCalculator();
     }
+
+    @Given("^Two input values, (-?\\d+) and (-?\\d+)$")
+    public void advTwoInputValuesAnd(int arg0, int arg1) {
+        value1 = arg0;
+        value2 = arg1;
+    }
+
+    @When("^I ([*/^]) the two values$")
+    public void iTheTwoValues(String operator) {
+        result = calculator.Calculate(value1, value2, operator);
+        System.out.println(result);
+    }
+
+    @Then("^I Expect the result (.+)$")
+    public void iExpectTheResult(String arg0) {
+        Assert.assertEquals(arg0, String.format("%,.1f", result));
+    }
+
 }
--- Exp1/src/test/resources/features/AdvanceCalculator.feature
+++ Exp1/src/test/resources/features/AdvanceCalculator.feature
@@ -10,3 +10,30 @@ Feature: AdvanceCalculator
     When I / the two values
     Then I Expect the result 0.5
 
+  Scenario: power two numbers
+    Given Two input values, 1 and 2
+    When I ^ the two values
+    Then I Expect the result 1.0
+
+  Scenario Outline: multiply two numbers
+    Given Two input values, <first> and <second>
+    When I <operator> the two values
+    Then I Expect the result <result>
+    Examples:
+      | first | second | operator | result |
+      | 1 | 12 | * | 12.0 |
+      | -2 | 4 | * | -8.0 |
+      | 2 | 3 | * | 6.0 |
+      | 3 | 0 | * | 0.0 |
+      | -6 | 1 | / | -6.0 |
+      | 6 | 2 | / | 3.0 |
+      | 6 | 4 | / | 1.5 |
+      | 6 | -1 | / | -6.0 |
+      | 2 | 2 | ^ | 4.0 |
+      | 2 | 0 | ^ | 1.0 |
+      | 2 | 1 | ^ | 2.0 |
+      | 1 | 2 | ^ | 1.0 |
+      | -1 | 2 | ^ | 1.0 |
+      | -1 | 3 | ^ | -1.0 |
+
+  Scenario: divide Two Number
